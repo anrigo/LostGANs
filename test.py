Normalization bug fix

test.py
@@ -8,27 +8,31 @@ from data.cocostuff_loader import *
 from data.vg import *
 from model.resnet_generator_v2 import *
 from utils.util import *
-from data.datasets import get_dataset
+from data.datasets import get_dataset, get_num_classes_and_objects
 
 
-def get_dataloader(dataset = 'coco', img_size=128, return_depth=False):
-    
+def get_dataloader(dataset='coco', img_size=128, return_depth=False):
+
     dataset = get_dataset(dataset, img_size, 'val', return_depth=return_depth)
 
     dataloader = torch.utils.data.DataLoader(
-                    dataset, batch_size=1,
-                    drop_last=True, shuffle=False, num_workers=1)
+        dataset, batch_size=1,
+        drop_last=True, shuffle=False, num_workers=1)
     return dataloader
 
 
 def main(args):
-    num_classes = 184 if args.dataset == 'coco' else 179
-    num_o = 8 if args.dataset == 'coco' else 31
+    num_classes, num_obj = get_num_classes_and_objects(args.dataset)
+
+    # output directory
+    args.sample_path = os.path.join(
+        args.sample_path, args.dataset + '-' + args.model)
 
     dataloader = get_dataloader(args.dataset, return_depth=args.use_depth)
 
     if args.use_depth:
-        netG = ResnetGeneratorDepth128(num_classes=num_classes, output_dim=3).cuda()
+        netG = ResnetGeneratorDepth128(
+            num_classes=num_classes, output_dim=3).cuda()
     else:
         netG = ResnetGenerator128(num_classes=num_classes, output_dim=3).cuda()
 
@@ -44,7 +48,8 @@ def main(args):
         new_state_dict[name] = v
 
     model_dict = netG.state_dict()
-    pretrained_dict = {k: v for k, v in new_state_dict.items() if k in model_dict}
+    pretrained_dict = {k: v for k,
+                       v in new_state_dict.items() if k in model_dict}
     model_dict.update(pretrained_dict)
     netG.load_state_dict(model_dict)
 
@@ -53,7 +58,7 @@ def main(args):
 
     if not os.path.exists(args.sample_path):
         os.makedirs(args.sample_path)
-    thres=2.0
+    thres = 2.0
 
     for idx, data in tqdm(enumerate(dataloader)):
 
@@ -64,17 +69,24 @@ def main(args):
             real_images, label, bbox = data
 
         real_images, label = real_images.cuda(), label.long().unsqueeze(-1).cuda()
-        z_obj = torch.from_numpy(truncted_random(num_o=num_o, thres=thres)).float().cuda()
-        z_im = torch.from_numpy(truncted_random(num_o=1, thres=thres)).view(1, -1).float().cuda()
+        z_obj = torch.from_numpy(truncted_random(
+            num_o=num_obj, thres=thres)).float().cuda()
+        z_im = torch.from_numpy(truncted_random(
+            num_o=1, thres=thres)).view(1, -1).float().cuda()
 
         if args.use_depth:
-            fake_images = netG.forward(z_obj, bbox.cuda(), z_im=z_im, y=label.squeeze(dim=-1), depths=depths)
+            fake_images = netG.forward(
+                z_obj, bbox.cuda(), z_im=z_im, y=label.squeeze(dim=-1), depths=depths)
         else:
-            fake_images = netG.forward(z_obj, bbox.cuda(), z_im, label.squeeze(dim=-1))
-        
-        result = normalize_tensor(fake_images[0].detach().permute(1, 2, 0)*0.5+0.5, (0,255)).type(torch.uint8).cpu().numpy()
+            fake_images = netG.forward(
+                z_obj, bbox.cuda(), z_im, label.squeeze(dim=-1))
 
-        imsave("{save_path}/sample_{idx}.jpg".format(save_path=args.sample_path, idx=idx), result)
+        # normalize from [-1,1] to [0,255]
+        result = ((fake_images[0].detach().permute(
+            1, 2, 0) + 1) / 2 * 255).type(torch.uint8).cpu().numpy()
+
+        imsave(
+            "{save_path}/sample_{idx}.jpg".format(save_path=args.sample_path, idx=idx), result)
 
 
 if __name__ == "__main__":
@@ -85,7 +97,9 @@ if __name__ == "__main__":
                         help='which epoch to load')
     parser.add_argument('--sample_path', type=str, default='samples',
                         help='path to save generated images')
-    parser.add_argument('--use_depth', type=bool, default=False,
-                        help='use depth')
+    parser.add_argument('--use_depth', action=argparse.BooleanOptionalAction,
+                        default=False, help='use depth information')
+    parser.add_argument('--model', type=str, default='baseline',
+                        help='short model name')
     args = parser.parse_args()
     main(args)

train.py
@@ -227,11 +227,10 @@ def main(args):
                 )
 
                 # put images in a grid tensor
-                # * 0.5 + 0.5 normalizes images to [0,1]
-                real_grid = make_grid(
-                    normalize_tensor(real_images, (0, 1)).cpu(), nrow=4)
-                fake_grid = make_grid(
-                    normalize_tensor(fake_images, (0, 1)).cpu(), nrow=4)
+                # all images are in [-1,1]
+                # normalize to [0,1] for visualization
+                real_grid = make_grid(((real_images + 1) / 2).cpu(), nrow=4)
+                fake_grid = make_grid(((fake_images + 1) / 2).cpu(), nrow=4)
 
                 wandb.log({
                     "real_images": wandb.Image(real_grid),
@@ -246,9 +245,10 @@ def main(args):
                         coord_box = scale_boxes(
                             bbox[jdx], train_data.image_size, 'coordinates', dtype=torch.int)
 
+                        # normalize form [-1,1] to [0,255]
+                        ann_img = (
+                            (real_images[jdx] + 1) / 2 * 255).type(torch.uint8).cpu()
                         # draw boxes
-                        ann_img = normalize_tensor(
-                            real_images[jdx].cpu(), (0, 255)).type(torch.uint8)
                         ann_img = draw_bounding_boxes(ann_img, coord_box)
 
                         # get depth layout
@@ -256,13 +256,13 @@ def main(args):
                             depths[jdx], train_data.image_size, bbox[jdx]).unsqueeze(0)
 
                         depth_results.extend([
-                            # normalize everything to [0,1]
-                            normalize_tensor(ann_img.type(
-                                torch.float32), (0, 1)),
+                            # normalize from [0,255] to [0,1]
+                            ann_img.type(torch.float32) / 255,
                             # already in [0,1]
                             torch.cat((depth_layout, depth_layout,
                                        depth_layout), 0).cpu(),
-                            normalize_tensor(fake_images[jdx], (0, 1)).cpu()
+                            # from [-1,1] to [0,1]
+                            ((fake_images[jdx] + 1) / 2).cpu()
                         ])
 
                     depth_grid = make_grid(depth_results, nrow=3)
@@ -298,8 +298,6 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='baseline',
                         help='short model name')
     args = parser.parse_args()
-
-    # os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
     # train params
     args.dataset = 'clevr'

utils/util.py
@@ -99,18 +99,19 @@ class VGGLoss(nn.Module):
         return loss
 
 
-def normalize_tensor(tensor: torch.Tensor, range: 'tuple[float, float]', eps: float = 1e-12) -> torch.Tensor:
+def normalize_tensor(tensor: torch.Tensor, to_: 'tuple[float, float]', from_: 'tuple[float, float]' = None, eps: float = 1e-12) -> torch.Tensor:
     '''
     Normalize tensor to the given range using min-max normalization (rescaling)
 
     Args:
         tensor: data tensor
-        range: tuple (min, max)
+        to: tuple (min, max) target range
+        from: tuple (min, max) input data range, if not specified max and min of the input will be used
         eps: small constant to avoid division by zero
     '''
 
-    min_, max_ = tensor.min(), tensor.max()
-    newmin, newmax = range
+    min_, max_ = (tensor.min(), tensor.max()) if from_ is None else from_
+    newmin, newmax = to_
     newmin, newmax = float(newmin), float(newmax)
     num = (tensor - min_) * (newmax - newmin)
     den = max((max_ - min_), eps)